421e9aa8bd23b96536ca619860825b2b6da77c1e
Ameliza <36745160+Ameliza@users.noreply.github.com>
2020-12-03 20:35:56 -0300

    Adicionando FLAG e ESC

---
+++ enquadramento.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+import poller
+import sys
+import time
+from poller import Callback
+from serial import Serial
+import enum
+
+# States
+IDLE = 0
+READ = 1
+ESCAPE = 2
+
+# Special
+FLAG = 0x7E  # ~
+ESC = 0x7D  # }
+
+
+class CallbackStdin(poller.Callback):
+
+    def __init__(self, tout, s):
+        Callback.__init__(self, sys.stdin, tout)
+        self.serial = s
+
+    def handle(self):  # Será um método def envia(self, msg) chamado pelo handle da Aplicação
+        msg = sys.stdin.buffer.readline()  # Envia a msg para o Enquadramento para ele enviar pela porta serial
+        frame = bytearray()
+        frame.append(FLAG)  # FLAG de inicio
+        
+        print(type(msg))
+
+        for byte in msg:
+            if (byte == 0x7E or byte == 0x7D):  # 7E -> 5E (^) / 7D -> ?(])
+                xor = byte ^ 0x20
+                frame.append(0x7D)
+                frame.append(xor)
+            else:
+                frame.append(byte)
+
+        frame.append(0x7E)  # FLAG de fim
+        self.transmitter(frame)
+
+    def handle_timeout(self):
+        print('Timeout !')
+
+    def transmitter(self, msg):
+        self.serial.write(msg)
+        print('Enviou: ', msg)
+
+
+class CallbackReception(poller.Callback):
+    t0 = time.time()
+
+    def __init__(self, tout, s):
+        poller.Callback.__init__(self, s, tout)
+        self.serial = s
+        self.frame = bytearray()
+
+    def handle(self):
+        self.frame = self.receiver()
+        print('Recebido: ', self.frame)
+
+    def handle_timeout(self):
+        print('Timer: t=', time.time() - CallbackReception.t0)
+
+    def receiver(self):
+        msg = self.serial.read()
+        # Notifica a Aplicação de que chegou mensagem pela serial e ela mostra no terminal
+        return msg
+
+    '''
+    def handle_state(self):
+    '''
+
+
+if __name__ == '__main__':
+    try:
+        s = Serial(sys.argv[1], 9600)
+    except Exception as e:
+        print('Não conseguiu acessar a porta serial', e)
+        sys.exit(0)
+
+    cb = CallbackStdin(30, s)
+    rx = CallbackReception(35, s)
+    sched = poller.Poller()
+
+    sched.adiciona(cb)
+    sched.adiciona(rx)
+
+    sched.despache()
