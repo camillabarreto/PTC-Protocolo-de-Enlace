aacbd24d70c5cb5dc097bc0f5d225ae9eeec6cdb
Ameliza <36745160+Ameliza@users.noreply.github.com>
2020-12-17 21:15:26 -0300

    Transmite o conteúdo do arquivo

--- application.py
+++ application.py
@@ -9,21 +9,33 @@ import sys
 
 class Application(Sublayer):
 
-    def __init__(self, porta_serial: Serial, tout: float):
-        Sublayer.__init__(self, porta_serial, tout)
+    def __init__(self, file, tout: float):
+        Sublayer.__init__(self, file, tout)
+        self.f = file
+        self.msg = bytearray()
+        self.i = 0
 
     def handle(self):
         '''Recebe os octetos do teclado, trata os dados
         e envia para a camada inferior'''
-
-        msg = sys.stdin.buffer.readline()  # Lê em bytes
-        msg = msg[:-1]  # Tirando o '/n' do final da mensagem lida
-        print('Application: send')
-        self.lowerLayer.send(msg)
+        
+        '''
+        linha = self.f.readline()
+        
+        while linha:
+            print(linha)
+            linha = self.f.readline()
+        self.f.close()
+        '''
+        
+        conteudo_arq = self.f.read()
+        self.f.close()
+        self.disable()
+        # print(conteudo_arq)
+        self.lowerLayer.send(conteudo_arq)
 
     def receive(self, msg):
         '''Recebe os octetos da camada inferior, trata os dados
-        e envia para o terminal'''
-        print('Application: receive')
-        print('mensagem: ', msg)
-
+        e envia para o terminal '''
+        # msg = msg[:-1]
+        print('mensagem: ', msg.decode('utf-8'))
---
+++ demo_protocol.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import poller
+import sys
+import time
+import argparse
+from poller import Callback
+from serial import Serial
+from application import Application
+from framing import Framing
+
+
+if __name__ == '__main__':
+    
+    # Visualização de ajuda
+    parser = argparse.ArgumentParser(description='Programa de Demonstração')
+    parser.add_argument('-s', '--serialPath', help='Porta serial onde ocorrerá a comunicação', type=str,
+                        dest='path', required=False, default="")
+    parser.add_argument('-f', '--fakeFile', help='Arquivo com o conteúdo que será transmitido', type=str,
+                        dest='file', required=False, default="")
+    parser.add_argument('-r', '--rate', help='Taxa de transmissão da porta serial', type=str,
+                        dest='rate', required=False, default="")
+    args = parser.parse_args()
+    
+    # Porta serial
+    try:
+        serial = Serial(args.path, args.rate)
+    except Exception as e:
+        print('Não conseguiu acessar a porta serial', e)
+        sys.exit(0)
+        
+    arq = open(args.file, 'r') # Abre o arquivo passado como argumento de linha
+    
+    # Callbacks
+    ap = Application(arq, 1)
+    fr = Framing(serial, 1)
+    ap.connect(fr, None)
+    fr.connect(None, ap)
+
+    # Poller
+    sched = poller.Poller()
+    sched.adiciona(ap)
+    sched.adiciona(fr)
+    sched.despache()
+
