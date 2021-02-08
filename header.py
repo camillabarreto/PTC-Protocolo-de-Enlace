354b20994ce1fc572f6f37c2861747a7d6ceaa66
Ameliza <36745160+Ameliza@users.noreply.github.com>
2021-02-08 08:46:16 -0300

    Corrigindo init() e get_data_frame()

--- header.py
+++ header.py
@@ -1,46 +1,49 @@
 #!/usr/bin/env python3
 
-class Header():
+MAX_BYTES = 128
+
+class Header:
+    
     def __init__(self):
-        self.control
-        self.reserved = 0x00
-        self.id_proto # número definido pela app
-        self.msg
-        self.fcs
+        self.seq = 0 # ARQ usa na recepção 
+        self.frame_type = 0 # ARQ usa na recepção
+        self.id_proto = 0x00 # ARQ usa na recepção
+        self.msg = b'' # ARQ usa na recepção
+        self.reservado = 0x00
         self.header = bytearray()
-        
-    def detach_frame(self, frame): # Recebe a msg e divide os campos de acordo
-        self.control = frame[0]
-        if(self.control == 0x00 or self.control == 0x08): # o header tem id_proto apenas se tipo data (bit7 = 0)
-            self.id_proto = frame[2]
-            self.msg = frame[3]
-        # self.fcs = ? O Enquadramento já retirou esse campo
-        
-    def get_frame(self, seq, id_proto, msg): # Envia o quadro com o cabeçalho anexado à mensagem
-        if(seq == 0):
-            self.control = 0x00
-        else:
-            self.control = 0x08
-        self.id_proto = id_proto
-        self.msg = msg
-        self.header.append(self.control)
-        self.header.append(self.reserved)
-        self.header.append(self.id_proto)
-        self.header.append(self.msg)
-        # self.header.append(self.fcs)
+    
+    def get_data_frame(self, seq, id_proto, msg):
+        if seq == 0: control = 0x00
+        else: control = 0x08
+        self.header.append(control)
+        self.header.append(self.reservado)
+        self.header.append(id_proto)
+        for byte in msg:
+            self.header.append(byte)
         return self.header
-        
-    def send_ack(self, seq):
-        if(seq == 0):
-            self.control = 0x80
-        else:
-            self.control = 0x88
-        self.header.append(self.control)
-        self.header.append(self.reserved)
-        # self.header.append(self.fcs)
+    
+    def get_ack_frame(self, seq):
+        if seq == 0: control = 0x80
+        else: control = 0x88
+        self.header.append(control)
+        self.header.append(self.reservado)
         return self.header
-        
-    def get_control(self):
-        return self.control
-        
-        
+    
+    def detach_frame(self, byte_frame):
+        control = byte_frame[0]
+        if control == 0x00:
+            self.seq = 0
+            self.frame_type = 0
+            self.id_proto = byte_frame[2]
+            self.msg = byte_frame[3:]
+        elif control == 0x08:
+            self.seq = 1
+            self.frame_type = 0
+            self.id_proto = byte_frame[2]
+            self.msg = byte_frame[3:]
+        elif control == 0x80:
+            self.seq = 0
+            self.frame_type = 1
+        elif control == 0x88:
+            self.seq = 1
+            self.frame_type = 1
