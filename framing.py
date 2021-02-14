550feb496231af93f8bc4c0028810cb05a81f806
Ameliza <36745160+Ameliza@users.noreply.github.com>
2021-02-14 16:09:35 -0300

    Adicionando verificação overflow na FSM

--- framing.py
+++ framing.py
@@ -5,6 +5,7 @@ from poller import Callback
 from sublayer import Sublayer
 from serial import Serial
 import crc
+from frame import Frame
 
 # States
 IDLE = 0
@@ -17,7 +18,7 @@ FLAG = 0x7E  # ~
 ESC = 0x7D  # }
 
 # Frame size limit
-MAX_BYTES = 128
+MAX_BYTES = 128 + 3     # cabeçalhos ARQ: control, id_proto, reservado
 
 
 '''A subcamada Framing é resposável por tratar as transmissões
@@ -31,30 +32,28 @@ e os octetos serão descartados'''
 
 class Framing(Sublayer):
 
-    def __init__(self, porta_serial: Serial, tout: float):
-        Sublayer.__init__(self, porta_serial, tout)
-        self.msg = bytearray()  # buffer
+    def __init__(self, serial_port: Serial, tout: float):
+        Sublayer.__init__(self, serial_port, tout)
+        self.buffer = bytearray()
         self.disable_timeout()
         self.current_state = IDLE
 
     def handle(self):
         byte = self.fd.read()  # Lendo 1 octeto da porta serial
         result = self.FSM(byte)  # FSM retorna True para recepção bem sucedida
-
         if (result):
-            if (len(self.msg) <= MAX_BYTES):
-                self.upperLayer.receive(bytes(self.msg)) # Envia mensagem para subcamada superior
+            frame_teste = Frame()
+            frame_teste.detach_frame(bytes(self.buffer))
+            self.upperLayer.receive(frame_teste)
 
-            else:
-                print('OVERFLOW! A mensagem tem ', len(self.msg), ' bytes.')
-
-            self.msg.clear()
+            self.buffer.clear()
 
     def handle_timeout(self):
         '''Trata um timeout associado a este callback'''
-        self.msg.clear()
+        self.buffer.clear()
         self.current_state = IDLE
         self.disable_timeout()
+        print('TIMEOUT!')
 
     def FSM(self, byte):
         switch = {
@@ -63,15 +62,14 @@ class Framing(Sublayer):
             READ: self.read,
             ESCAPE: self.escape
         }
-
-        func = switch.get(self.current_state, lambda: None) # Executa a função do estado atual
+        func = switch.get(self.current_state, lambda: None)
         return func(byte)
 
     def idle(self, byte):
         if (byte[0] == FLAG):
             self.current_state = INIT
-
-        self.enable_timeout()
+            self.enable_timeout()
+            
         return False
 
     def init(self, byte):
@@ -82,8 +80,13 @@ class Framing(Sublayer):
             self.current_state = ESCAPE
 
         else:
-            self.msg.append(byte[0])
-            self.current_state = READ
+            if (len(self.buffer) <= MAX_BYTES):
+                self.buffer.append(byte[0])
+                self.current_state = READ
+            else:
+                self.current_state = IDLE
+                print('1 OVERFLOW! A mensagem tem', MAX_BYTES,'bytes.')
+                self.buffer.clear()
 
         self.reload_timeout()
         return False
@@ -92,65 +95,65 @@ class Framing(Sublayer):
         if (byte[0] == FLAG):
             self.current_state = IDLE
             self.disable_timeout()
-            fcs = crc.CRC16(self.msg)
-
+            fcs = crc.CRC16(self.buffer)
             if fcs.check_crc():
-                self.msg = self.msg[:-2]
+                self.buffer = self.buffer[:-2]
                 return True
+            else: self.buffer.clear()
 
         elif (byte[0] == ESC):
             self.current_state = ESCAPE
 
         else:
-            self.msg.append(byte[0])
-            self.current_state = READ
+            if (len(self.buffer) <= MAX_BYTES):
+                self.buffer.append(byte[0])
+                self.current_state = READ
+            else:
+                self.current_state = IDLE
+                print('2 OVERFLOW! A mensagem tem', len(self.buffer), 'bytes.')
+                self.buffer.clear()
 
         self.reload_timeout()
         return False
 
     def escape(self, byte):
         if (byte[0] == FLAG or byte[0] == ESC):
-            self.msg.clear()
+            self.buffer.clear()
+            self.disable_timeout()
             self.current_state = IDLE
 
         else:
-            byte = byte[0] ^ 0x20
-            self.msg.append(byte)
-            self.current_state = READ
+            if (len(self.buffer) <= MAX_BYTES):
+                byte = byte[0] ^ 0x20
+                self.buffer.append(byte)
+                self.current_state = READ
+            else:
+                self.current_state = IDLE
+                print('3 OVERFLOW! A mensagem tem', MAX_BYTES, 'bytes.')
+                self.buffer.clear()
 
         self.reload_timeout()
         return False
 
-    def send(self, msg):
+    def send(self, fr):
         '''Recebe os octetos da camada superior, trata os dados
-        e envia pela porta serial'''
+        e envia pela porta serial '''
 
-        fcs = crc.CRC16(msg)
-        
+        fcs = crc.CRC16(fr.header)
+        fr.header = fcs.gen_crc()
+
         frame = bytearray()
+        frame.append(FLAG)
 
-        if (len(msg) <= MAX_BYTES):
-            msg = fcs.gen_crc()  # Anexa na mensagem o valor de FCS
+        for byte in fr.header:
+            if (byte == FLAG or byte == ESC):
+                xor = byte ^ 0x20
+                frame.append(ESC)
+                frame.append(xor)
 
-            # GERAR ERROR PROPOSITAIS NA TRANSMISSÃO
-            # msg = msg[:-1] # remove o ultimo byte
-            # msg.reverse() # inverte os bytes
-            # msg[0] = 99  # alterando o primeiro byte
-            
-            frame.append(FLAG)
+            else:
+                frame.append(byte)
 
-            for byte in msg:
-                if (byte == FLAG or byte == ESC):
-                    xor = byte ^ 0x20
-                    frame.append(ESC)
-                    frame.append(xor)
-
-                else:
-                    frame.append(byte)
-
-            frame.append(FLAG)
-            self.fd.write(bytes(frame)) # Envia mensagem pela porta serial
-
-        else:
-            print('OVERFLOW! A mensagem tem ', len(msg), ' bytes.')
+        frame.append(FLAG)
+        self.fd.write(bytes(frame))  # Envia mensagem pela porta serial
 
